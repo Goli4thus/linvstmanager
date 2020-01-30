df0a46d3874acb3a1824d0c9ea02e136cd913e7c
Goli4thus <goli@posteo.net>
2020-01-30 22:24:39 +0100

    Added enabling of debug message output via cmake argument.

---
+++ src/config.h
@@ -0,0 +1,18 @@
+// This file has been generated automatically by CMake. Do not edit it manually, as all
+// changes will be overwritten in the future.
+
+#ifndef CORE_CONFIG_H
+#define CORE_CONFIG_H
+
+#define D_PROJECT_NAME "LinVstManager"
+
+// Program version
+#define D_VERSION_MAJOR 1
+#define D_VERSION_MINOR 0
+#define D_VERSION_PATCH 0
+#define D_VERSION_STRING "1.0.0"
+
+// Various constants
+#define D_OUTPUT_DEBUG_MSG 1
+
+#endif // CORE_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <stdlib.h>
+
+void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    const char *file = context.file ? context.file : "";
+    const char *function = context.function ? context.function : "";
+    switch (type) {
+        case QtDebugMsg:
+            if (false) {
+                fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+            }
+        break;
+        case QtInfoMsg:
+            fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+        case QtWarningMsg:
+            fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+        case QtCriticalMsg:
+            fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+        case QtFatalMsg:
+            fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(customMessageHandler);
     QApplication a(argc, argv);
     MainWindow w;
     w.resize(800, 550);
